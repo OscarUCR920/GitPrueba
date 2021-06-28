d3f73eae88a38774de9c014587c1c723010e50fb
Oscar Noguera <onoguera@soluciones506.com>
2021-06-27 18:08:54 -0600

    cambios oscar 2

---
+++ GitPrueba/Models/Gordita.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitPrueba.Models
+{
+    public class Gordita
+    {
+			String nombre = '';
+			String nombre = '';
+    }
+}
---
+++ GitPrueba/Models/Gordito.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitPrueba.Models
+{
+    public class Gordito
+    {
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+			String nombre = '';
+    }
+}
